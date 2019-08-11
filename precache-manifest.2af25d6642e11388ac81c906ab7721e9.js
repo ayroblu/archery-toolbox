e528acc0ae5aa72f8cf6520d53324113c9f54aac
Ben Lu <ayroblu@gmail.com>
2019-08-11 20:23:31 +0100

    Updates

---
+++ precache-manifest.2af25d6642e11388ac81c906ab7721e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d141ec6510f1010240165bed8f5f9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a975250913f87c067a57",
+    "url": "/static/css/main.c3e7ce7e.chunk.css"
+  },
+  {
+    "revision": "55d0118a8c13b29972df",
+    "url": "/static/js/2.9a2d3bba.chunk.js"
+  },
+  {
+    "revision": "a975250913f87c067a57",
+    "url": "/static/js/main.8e9d9b76.chunk.js"
+  },
+  {
+    "revision": "1b72d40fb700d9a2d999",
+    "url": "/static/js/runtime~main.00e7bf8a.js"
+  }
+]);
