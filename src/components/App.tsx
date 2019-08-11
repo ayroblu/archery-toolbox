163851a16e63433003cb360b9ade8f985e50ba6d
Ben Lu <ayroblu@gmail.com>
2019-08-11 02:42:48 +0100

    Updating: Change index, add types for routes

---
+++ src/components/App.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { withRouter, Switch, Route } from "react-router-dom";
+//import { connect } from 'react-redux'
+//import { bindActionCreators } from 'redux'
+
+import Layout from "../components/Layout";
+//import NoMatch from '../components/NoMatch'
+
+//import * as userActions from '../actions/user'
+
+import "./App.css";
+
+class AppComp extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/" component={Layout} />
+      </Switch>
+    );
+  }
+}
+
+export const App = withRouter(AppComp);
